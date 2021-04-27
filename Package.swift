a57756ef79c4677ce01b5c452360e0084cc2fa20
Mikhail Nikanorov <fobozsb@gmail.com>
2021-04-26 17:27:36 -0700

    Update SwiftPM section

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     ),
     .binaryTarget(
        name: "Lokalise",
-       url: "https://github.com/lokalise/lokalise-ios-framework/raw/master/Lokalise.xcframework.zip",
-       checksum: "2d26c78452fed3b29889d6c8e5bebc520b3d2acf400c82dde8d29100bc67ef7e"
+       url: "https://github.com/lokalise/lokalise-ios-framework/releases/download/0.10.2/Lokalise.xcframework.0.10.2.zip",
+       checksum: "9e7c9ede0cc2092d3b86538e0b5c2244ed1d2f32323188a45f1dd06366a68be6"
     )
   ]
 )
