e9048e2d052edc12bb14eb3e5211234f6df36e9d
Fedya Levkin <fedya@lokalise.com>
2021-05-13 21:04:52 +0300

    SILV-1 Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "Lokalise",
+  platforms: [.iOS(.v9)],
+  products: [
+    .library(name: "Lokalise",
+             targets: ["Lokalise"])
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .binaryTarget(
+       name: "Lokalise",
+       url: "https://github.com/lokalise/lokalise-ios-framework/releases/download/0.10.2/Lokalise.xcframework.0.10.2.zip",
+       checksum: "9e7c9ede0cc2092d3b86538e0b5c2244ed1d2f32323188a45f1dd06366a68be6"
+    )
+  ]
+)
